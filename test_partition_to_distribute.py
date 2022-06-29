cace7ea1fb9bc19cba4d86dd7b61428bfd0f1260
Bihshtein <alexbihsh@gmail.com>
2022-06-29 17:23:09 +0300

    whoa

--- test_partition_to_distribute.py
+++ test_partition_to_distribute.py
@@ -1,26 +1,38 @@
 import unittest
 
-def func(arr, k):
-    return max(_func(arr,k))
 
-def _func(arr, k):
-    arr_sum = sum(arr)
-    count = len(arr)
+def numberOfPainters(arr, maxLen):
+    total = 0
+    numPainters = 1
+    for i in arr:
+        total += i
+        if total > maxLen:
+            total = i
+            numPainters += 1
 
-    if k == 1:
-        return [arr_sum]
-
-    curr_sum = 0
-    for i in range(count -1, 0, -1):
-        curr_sum += arr[i]
-        if (((arr[i-1])+curr_sum)*k > arr_sum ) and  curr_sum > arr[i-1]:
-            return [curr_sum] + _func(arr[:i], k-1)
+    return numPainters
 
 
+def partition(arr, k):
+    max_element = max(arr)
+    sum_arr = sum(arr)
+    while max_element < sum_arr:
+        mid = max_element + (sum_arr - max_element) // 2
+        requiredPainters = numberOfPainters(arr, mid)
+        if requiredPainters <= k:
+            sum_arr = mid
+        else:
+            max_element = mid + 1
+    return max_element
 
 
 class Tests(unittest.TestCase):
     def test_i(self):
-        self.assertEqual(35, func([5,10,30,20,15], 3))
-        self.assertEqual(60, func([10,20,30,40], 2))
+        self.assertEqual(2, partition([2, 1, 1, 1], 3))
+        self.assertEqual(35, partition([5, 10, 30, 20, 15], 3))
+        self.assertEqual(1, partition([1, 1, 1, 1], 4))
+        self.assertEqual(2, partition([1, 1, 1, 1], 3))
+        self.assertEqual(60, partition([10, 20, 30, 40], 2))
+
+
 
